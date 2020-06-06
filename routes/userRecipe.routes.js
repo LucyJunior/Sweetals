a70354cee7510b267608dc70b288ccc0b245bff9
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-06-06 16:18:03 +0200

    mistake

--- routes/userRecipe.routes.js
+++ routes/userRecipe.routes.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
 
-router.get('/',(req,res) => res.render('users/userRecipe', {title:boum}) )
+router.get('/',(req,res) => res.render('users/userRecipe', {title: 'boum' }) )
 
 module.exports = router; 
