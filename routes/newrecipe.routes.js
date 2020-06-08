a05f2482debdb92f06bf78cd194d0ea4c299d860
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-06-08 16:54:01 +0200

    create new recipe connected to Mongo Done

--- routes/newrecipe.routes.js
+++ routes/newrecipe.routes.js
@@ -3,8 +3,33 @@ const router = express.Router();
 
 let RecipeModel = require ('../models/Recipe.model')
 
-router.get('/', (req, res) =>
-res.render('users/newRecipe', {title: 'new recipe of the user'}))
+router.get('/', (req, res, next) => {
+  res.render('users/newRecipe')
+  // i will pass all the info from flower list here, with .find() flowers
+});
 
+router.post('/', (req, res, next) => {
+  const {
+    title,
+    cookingTime,
+    numberOfPeople,
+    description,
+  } = req.body;
+  RecipeModel.create({
+    title : title,
+    cookingTime: cookingTime,
+    numberOfPeople :numberOfPeople,
+    description : description,
+    })
+    .then(() => {
+      res.redirect('/userRecipe')
+      //because it's redirect I need the slash before
+    })
+    .catch((error) => {
+      console.log("error", error)
+      res.render('users/newRecipe')
+    })
+
+});
 
 module.exports = router;
